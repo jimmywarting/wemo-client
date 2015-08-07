8b7156d94462d33916c0456551951f1472f61f3e
Timon Reinhard <hallo@timonreinhard.de>
2015-08-07 17:29:05 +0200

    Improve example

--- example/index.js
+++ example/index.js
@@ -1,13 +1,23 @@
 var Wemo = require('../index');
 
 var wemo = new Wemo();
-wemo.discover(function(device){
+
+function foundDevice(device){
+  console.log('Wemo Device Found: %j', device);
   var client = this.client(device);
+
   client.on('BinaryState', function(event){
-    console.log(event);
+    console.log('Binary State: %j', event);
   });
+
   client.subscribe('urn:Belkin:service:basicevent:1');
   client.setBinaryState(1);
-});
+}
 
-//setInterval(wemo.discover, 5000);
+// Inital discovery
+wemo.discover(foundDevice);
+
+// Repeat discovery as some devices may appear late
+setInterval(function(){
+  wemo.discover(foundDevice);
+}, 15000);
