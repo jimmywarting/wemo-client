bc193d2d2103bb513d720e47fb700b44aeea4135
Timon Reinhard <hallo@timonreinhard.de>
2015-08-10 22:11:33 +0200

    Add example for Wemo Motion

---
+++ examples/motion.js
@@ -0,0 +1,19 @@
+var Wemo = require('../index');
+
+var wemo = new Wemo();
+
+function foundDevice(device){
+  if (device.deviceType === 'urn:Belkin:device:sensor:1') {
+    console.log('Wemo Motion found: %s', device.friendlyName);
+
+    var client = this.client(device);
+    client.on('binaryState', function(value){
+      if (value === '1') {
+        console.log('Wemo Motion "%s" detected motion', this.device.friendlyName);
+      }
+    });
+  }
+
+}
+
+wemo.discover(foundDevice);
